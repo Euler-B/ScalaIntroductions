61d861fa352134878f3286a367a308c1e97b3023
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2023-04-08 21:02:56 -0400

    greeting in scala

---
+++ HolaMundo.scala
@@ -0,0 +1,8 @@
+object HolaMundo {
+  def main(args: Array[String]): Unit = {
+    println("================Saludo================")
+    println("Hello, mundo, y ciudad de Merida")
+    println("Los saludos desde los Chorros de Milla")
+    println("=======================================")
+  }
+}
